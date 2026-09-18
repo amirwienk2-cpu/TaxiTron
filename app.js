68c2ca69673ce75084dbdbc023996e5f1b63a811
Misha <mishaasl1996@gmail.com>
2026-09-18 02:04:39 +0200

    Chat: neues groesseres Admin-Badge (adminnew.png)

--- app.js
+++ app.js
@@ -1142,7 +1142,7 @@
     nameEl.className = 'chat-msg-name' + (msg.isAdmin ? ' admin' : '');
     if (msg.isAdmin) {
       const badge = document.createElement('img');
-      badge.src = 'sprites/admin.png';
+      badge.src = 'sprites/adminnew.png';
       badge.alt = t('chatAdminTag');
       badge.title = t('chatAdminTag');
       badge.className = 'chat-admin-badge';
